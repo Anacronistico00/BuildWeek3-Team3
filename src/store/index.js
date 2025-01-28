014f7ba442f9587984ba74b345d7014dc380665b
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-01-28 18:58:00 +0100

    Commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import mainReducer from '../reducers';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import experiencesReducer from '../reducers/experiences';
+import tokenReducer from '../reducers/token';
+import userReducer from '../reducers/user';
+import setProfileReducer from '../reducers/profile';
+
+const mainReducer = combineReducers({
+  experiences: experiencesReducer,
+  token: tokenReducer,
+  user: userReducer,
+  profile: setProfileReducer,
+});
 
 const store = configureStore({
   reducer: mainReducer,

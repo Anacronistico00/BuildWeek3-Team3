7cff08b9879a2b8c2eb31ab40d2db2770f299dc4
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-01-28 09:26:15 +0100

    Commit1-Day2

--- src/components/ModalPostComponent.jsx
+++ src/components/ModalPostComponent.jsx
@@ -1,46 +1,59 @@
 import { Button, Modal, Row, Col, FloatingLabel, Form } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
-const ModalPost = ({ show, handleClose, image, name }) => {
+const ModalPost = ({ show, handleClose }) => {
+  const profile = useSelector((state) => state.profile);
   return (
-    <Modal show={show} onHide={handleClose} className='modalePost modal-lg'>
-      <Modal.Header closeButton>
-        <Modal.Title>
-          <Button className='p-2 transparent-button '>
-            <Row className='align-items-center hover-row '>
-              <Col md={2}>
-                <img src='../../public/assets/images/avatar.png' alt='Profile' className='circular-image' style={{width:'40px'}} />
-              </Col>
-              <Col className='ms-4 d-flex align-items-center justify-content-between'>
-                <div>
-                  <h4 className='mb-0 text-black text-start ms-2'>{name}</h4>
-                  <p className='mb-0 fs-6 text-black text-start ms-2'>
-                    Pubblica: Chiunque
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          </Button>
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <FloatingLabel
-          controlId='floatingTextarea2'
-          label='Di cosa vorresti Parlare?'
-        >
-          <Form.Control
-            as='textarea'
-            placeholder='Di cosa vorresti Parlare?'
-            style={{ height: '250px' }}
-          />
-        </FloatingLabel>
-      </Modal.Body>
+    <>
+      {profile && (
+        <Modal show={show} onHide={handleClose} className='modalePost modal-lg'>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <Button variant='transparent' className='p-2 transparent-button '>
+                <Row className='align-items-center hover-row '>
+                  <Col md={2}>
+                    <img
+                      src={profile.image}
+                      alt='Profile'
+                      className='circular-image'
+                      style={{ width: '40px', borderRadius: '50%' }}
+                    />
+                  </Col>
+                  <Col className='ms-4 d-flex align-items-center justify-content-between'>
+                    <div>
+                      <h4 className='mb-0 text-black text-start ms-2'>
+                        {profile.name} {profile.surname}
+                      </h4>
+                      <p className='mb-0 fs-6 text-black text-start ms-2'>
+                        Pubblica: Chiunque
+                      </p>
+                    </div>
+                  </Col>
+                </Row>
+              </Button>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <FloatingLabel
+              controlId='floatingTextarea2'
+              label='Di cosa vorresti Parlare?'
+            >
+              <Form.Control
+                as='textarea'
+                placeholder='Di cosa vorresti Parlare?'
+                style={{ height: '250px' }}
+              />
+            </FloatingLabel>
+          </Modal.Body>
 
-      <Modal.Footer>
-        <Button variant='primary' onClick={handleClose}>
-          Salva modifiche
-        </Button>
-      </Modal.Footer>
-    </Modal>
+          <Modal.Footer>
+            <Button variant='primary' onClick={handleClose}>
+              Salva modifiche
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+    </>
   );
 };
 
--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -18,111 +18,120 @@ import {
   PeopleFill,
   Search,
 } from 'react-bootstrap-icons';
+import { useSelector } from 'react-redux';
 
 const NavBarComponent = function () {
+  const profile = useSelector((state) => state.profile);
+
   return (
-    <Navbar className='p-0' fixed='top' bg='light' expand='lg'>
-      <Container>
-        <Row className='w-100'>
-          <Col xs={2} md={3} className='d-flex align-items-center'>
-            <Navbar.Brand href='#'>
-              <img
-                src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/72px-LinkedIn_icon.svg.png'
-                width='36'
-                height='36'
-                className='d-inline-block align-top'
-                alt='LinkedIn logo'
-              />
-            </Navbar.Brand>
-
-            {/* Barra di ricerca */}
-            <Form className='mr-auto d-flex align-items-center'>
-              <div className='input-group'>
-                <Search className='search-icon' />
-                <FormControl
-                  type='text'
-                  placeholder='Search'
-                  className='d-none d-md-block me-2'
-                />
-              </div>
-            </Form>
-          </Col>
-          <Col xs={8} md={6}>
-            {/* Menu di navigazione */}
-            <Nav className='d-flex flex-row align-items-center justify-content-evenly'>
-              <Nav.Link
-                href='#home'
-                className='d-flex flex-column align-items-center mx-2'
-              >
-                <HouseDoorFill className='HomeIcons' />
-                <p className='SubsIcon d-none d-md-block'>Home</p>
-              </Nav.Link>
-
-              <Nav.Link
-                href='#network'
-                className='d-flex flex-column align-items-center mx-2'
-              >
-                <PeopleFill className='HomeIcons' />
-                <p className='SubsIcon d-none d-md-block'>Rete</p>
-              </Nav.Link>
-
-              <Nav.Link
-                href='#jobs'
-                className='d-flex flex-column align-items-center mx-2'
-              >
-                <BriefcaseFill className='HomeIcons' />
-                <p className='SubsIcon d-none d-md-block'>Lavoro</p>
-              </Nav.Link>
-
-              <Nav.Link
-                href='#messaging'
-                className='d-flex flex-column align-items-center mx-2'
-              >
-                <ChatDotsFill className='HomeIcons' />
-                <p className='SubsIcon d-none d-md-block'>Messaggistica</p>
-              </Nav.Link>
-
-              <Nav.Link
-                href='#notifications'
-                className='d-flex flex-column align-items-center mx-2'
-              >
-                <Bell className='HomeIcons' />
-                <p className='SubsIcon d-none d-md-block'>Notifiche</p>
-              </Nav.Link>
-
-              {/* Profilo Utente */}
-              <div className='d-flex justify-content-center align-items-center'>
-                <div className='ImgProfile'>
+    <>
+      {profile && (
+        <Navbar className='p-0' fixed='top' bg='light' expand='lg'>
+          <Container>
+            <Row className='w-100'>
+              <Col xs={2} md={3} className='d-flex align-items-center'>
+                <Navbar.Brand href='#'>
                   <img
-                    src='https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg'
-                    alt='imagine profilo'
-                    className='rounded-circle'
-                    width='20'
-                    height='20'
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/72px-LinkedIn_icon.svg.png'
+                    width='36'
+                    height='36'
+                    className='d-inline-block align-top'
+                    alt='LinkedIn logo'
                   />
-                </div>
-                <span className=' nameTab'>Tu</span>
-                <NavDropdown>
-                  <NavDropdown.Item href='#profile'>Profilo</NavDropdown.Item>
-                  <NavDropdown.Item href='#settings'>
-                    Impostazioni
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href='#logout'>Esci</NavDropdown.Item>
-                </NavDropdown>
-              </div>
-            </Nav>
-          </Col>
-          {/* Sezione "Per le Aziende" */}
-          <Col xs={2} md={3} className='border-start ms-0'>
-            <Nav.Link href='#work' className='text-center'>
-              <Grid3x3 className='HomeIcons mb-0 mt-2' />
-              <p className='SubsIcon d-none d-md-block'>Per le aziende</p>
-            </Nav.Link>
-          </Col>
-        </Row>
-      </Container>
-    </Navbar>
+                </Navbar.Brand>
+
+                {/* Barra di ricerca */}
+                <Form className='mr-auto d-flex align-items-center'>
+                  <div className='input-group'>
+                    <Search className='search-icon' />
+                    <FormControl
+                      type='text'
+                      placeholder='Search'
+                      className='d-none d-md-block me-2'
+                    />
+                  </div>
+                </Form>
+              </Col>
+              <Col xs={8} md={6}>
+                {/* Menu di navigazione */}
+                <Nav className='d-flex flex-row align-items-center justify-content-evenly'>
+                  <Nav.Link
+                    href='#home'
+                    className='d-flex flex-column align-items-center mx-2'
+                  >
+                    <HouseDoorFill className='HomeIcons' />
+                    <p className='SubsIcon d-none d-md-block'>Home</p>
+                  </Nav.Link>
+
+                  <Nav.Link
+                    href='#network'
+                    className='d-flex flex-column align-items-center mx-2'
+                  >
+                    <PeopleFill className='HomeIcons' />
+                    <p className='SubsIcon d-none d-md-block'>Rete</p>
+                  </Nav.Link>
+
+                  <Nav.Link
+                    href='#jobs'
+                    className='d-flex flex-column align-items-center mx-2'
+                  >
+                    <BriefcaseFill className='HomeIcons' />
+                    <p className='SubsIcon d-none d-md-block'>Lavoro</p>
+                  </Nav.Link>
+
+                  <Nav.Link
+                    href='#messaging'
+                    className='d-flex flex-column align-items-center mx-2'
+                  >
+                    <ChatDotsFill className='HomeIcons' />
+                    <p className='SubsIcon d-none d-md-block'>Messaggistica</p>
+                  </Nav.Link>
+
+                  <Nav.Link
+                    href='#notifications'
+                    className='d-flex flex-column align-items-center mx-2'
+                  >
+                    <Bell className='HomeIcons' />
+                    <p className='SubsIcon d-none d-md-block'>Notifiche</p>
+                  </Nav.Link>
+
+                  {/* Profilo Utente */}
+                  <div className='d-flex justify-content-center align-items-center'>
+                    <div className='ImgProfile'>
+                      <img
+                        src={profile.image}
+                        alt='imagine profilo'
+                        className='rounded-circle'
+                        width='20'
+                        height='20'
+                      />
+                    </div>
+                    <span className=' nameTab'>Tu</span>
+                    <NavDropdown>
+                      <NavDropdown.Item href='#profile'>
+                        Profilo
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href='#settings'>
+                        Impostazioni
+                      </NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href='#logout'>Esci</NavDropdown.Item>
+                    </NavDropdown>
+                  </div>
+                </Nav>
+              </Col>
+              {/* Sezione "Per le Aziende" */}
+              <Col xs={2} md={3} className='border-start ms-0'>
+                <Nav.Link href='#work' className='text-center'>
+                  <Grid3x3 className='HomeIcons mb-0 mt-2' />
+                  <p className='SubsIcon d-none d-md-block'>Per le aziende</p>
+                </Nav.Link>
+              </Col>
+            </Row>
+          </Container>
+        </Navbar>
+      )}
+    </>
   );
 };
 

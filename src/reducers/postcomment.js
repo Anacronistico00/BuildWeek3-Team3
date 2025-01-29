c1d882892bfd6ff38c5924b0b28086f354396eaf
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-01-29 16:15:35 +0100

    Commit

--- src/reducers/postcomment.js
+++ src/reducers/postcomment.js
@@ -1,15 +1,15 @@
 const initialState = {
-  text: "",
-}
+  text: '',
+};
 
 const postCommentReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SUBMIT_COMMENT:
+    case 'SUBMIT_COMMENT':
       return {
         ...state,
         text: action.payload,
-      }
+      };
   }
-}
+};
 
-export default postCommentReducer
+export default postCommentReducer;

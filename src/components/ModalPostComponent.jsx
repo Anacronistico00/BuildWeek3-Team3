207495d536f38179948dcfce9f2cd35837fb3d8e
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-01-29 15:58:24 +0100

    submit

--- src/components/ModalPostComponent.jsx
+++ src/components/ModalPostComponent.jsx
@@ -1,39 +1,40 @@
-import { Button, Modal, Row, Col, FloatingLabel, Form } from "react-bootstrap"
-import { useDispatch, useSelector } from "react-redux"
-import postComment from "../actions/postcomment"
+import { Button, Modal, Row, Col, FloatingLabel, Form } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import postComment from '../actions/postcomment';
 
 const ModalPost = ({ show, handleClose }) => {
-  const profile = useSelector((state) => state.profile)
-  const dispatch = useDispatch()
+  const profile = useSelector((state) => state.profile);
+  const dispatch = useDispatch();
+  const text = useSelector((state) => state.text);
 
-  const token = useSelector((state) => state.token)
+  const token = useSelector((state) => state.token);
 
   const handleSubmit = () => {
-    dispatch(postComment(text, token))
-  }
+    dispatch(postComment(token.token, text.text));
+  };
 
   return (
     <>
       {profile.profile && (
-        <Modal show={show} onHide={handleClose} className="modalePost modal-lg">
+        <Modal show={show} onHide={handleClose} className='modalePost modal-lg'>
           <Modal.Header closeButton>
             <Modal.Title>
-              <Button variant="transparent" className="p-2 transparent-button ">
-                <Row className="align-items-center hover-row ">
+              <Button variant='transparent' className='p-2 transparent-button '>
+                <Row className='align-items-center hover-row '>
                   <Col md={2}>
                     <img
                       src={profile.profile.image}
-                      alt="Profile"
-                      className="circular-image"
-                      style={{ width: "40px", borderRadius: "50%" }}
+                      alt='Profile'
+                      className='circular-image'
+                      style={{ width: '40px', borderRadius: '50%' }}
                     />
                   </Col>
-                  <Col className="ms-4 d-flex align-items-center justify-content-between">
+                  <Col className='ms-4 d-flex align-items-center justify-content-between'>
                     <div>
-                      <h4 className="mb-0 text-black text-start ms-2">
+                      <h4 className='mb-0 text-black text-start ms-2'>
                         {profile.profile.name} {profile.profile.surname}
                       </h4>
-                      <p className="mb-0 fs-6 text-black text-start ms-2">
+                      <p className='mb-0 fs-6 text-black text-start ms-2'>
                         Pubblica: Chiunque
                       </p>
                     </div>
@@ -44,33 +45,33 @@ const ModalPost = ({ show, handleClose }) => {
           </Modal.Header>
           <Modal.Body>
             <FloatingLabel
-              controlId="floatingTextarea2"
-              label="Di cosa vorresti Parlare?"
+              controlId='floatingTextarea2'
+              label='Di cosa vorresti Parlare?'
             >
               <Form.Control
-                as="textarea"
-                placeholder="Di cosa vorresti Parlare?"
-                style={{ height: "250px" }}
+                as='textarea'
+                placeholder='Di cosa vorresti Parlare?'
+                style={{ height: '250px' }}
                 value={text.text}
                 onChange={(e) => {
                   dispatch({
-                    type: SUBMIT_COMMENT,
+                    type: 'SUBMIT_COMMENT',
                     payload: e.target.value,
-                  })
+                  });
                 }}
               />
             </FloatingLabel>
           </Modal.Body>
 
           <Modal.Footer>
-            <Button variant="primary" onClick={handleSubmit}>
+            <Button variant='primary' onClick={handleSubmit}>
               Salva modifiche
             </Button>
           </Modal.Footer>
         </Modal>
       )}
     </>
-  )
-}
+  );
+};
 
-export default ModalPost
+export default ModalPost;

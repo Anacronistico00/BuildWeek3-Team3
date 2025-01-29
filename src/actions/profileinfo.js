25524a0527542c6fe2ef406c33fb1db94fd592c1
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-01-29 16:10:18 +0100

    Commit

--- src/actions/profileinfo.js
+++ src/actions/profileinfo.js
@@ -1,30 +1,30 @@
-const SET_PROFILE = "SET_PROFILE"
+export const SET_PROFILE = 'SET_PROFILE';
 
 export const setProfile = (profile) => ({
   type: SET_PROFILE,
   payload: profile,
-})
+});
 
 export const getProfileInfo = (token) => {
   return async (dispatch) => {
     try {
       const response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/profile/me",
+        'https://striveschool-api.herokuapp.com/api/profile/me',
         {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         }
-      )
+      );
       if (!response.ok) {
-        throw new Error("Network response was not ok")
+        throw new Error('Network response was not ok');
       }
-      const data = await response.json()
-      console.log(data)
+      const data = await response.json();
+      console.log(data);
 
-      dispatch(setProfile(data))
+      dispatch(setProfile(data));
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
-}
+  };
+};

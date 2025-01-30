644e99fdb02e5111ab526d44c8bd76049eea3aae
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-01-30 12:41:35 +0100

    Commit

--- src/actions/Jobs.js
+++ src/actions/Jobs.js
@@ -1,31 +1,9 @@
-export const GET_JOBS = "GET_JOBS";
+export const GET_JOBS = 'GET_JOBS';
 
 export const GetJobs = () => async (dispatch) => {
   try {
     const response = await fetch(
-      "https://strive-benchmark.herokuapp.com/api/jobs?search=query"
-    );
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    console.log(data.data);
-
-    dispatch({
-      type: GET_JOBS,
-      payload: data.data,
-    });
-  } catch (error) {
-    console.error("Errore nel recupero dei lavori:", error);
-  }
-};
-
-export const CATEGORY_SEARCH = 'CATEGORY_SEARCH';
-
-export const CategorySearch = (category) => async (dispatch) => {
-  try {
-    const response = await fetch(
-      `https://strive-benchmark.herokuapp.com/api/jobs?category=${category}&limit=10`
+      'https://strive-benchmark.herokuapp.com/api/jobs?search=query'
     );
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -38,6 +16,28 @@ export const CategorySearch = (category) => async (dispatch) => {
       payload: data.data,
     });
   } catch (error) {
-    console.error('Errore nel recupero dei lavori:', error);  
+    console.error('Errore nel recupero dei lavori:', error);
+  }
+};
+
+export const CATEGORY_SEARCH = 'CATEGORY_SEARCH';
+
+export const CategorySearch = (category) => async (dispatch) => {
+  try {
+    const response = await fetch(
+      `https://strive-benchmark.herokuapp.com/api/jobs?category=${category}&limit=20`
+    );
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log(data.data);
+
+    dispatch({
+      type: GET_JOBS,
+      payload: data.data,
+    });
+  } catch (error) {
+    console.error('Errore nel recupero dei lavori:', error);
   }
 };
--- src/components/JobsComponent.jsx
+++ src/components/JobsComponent.jsx
@@ -1,22 +1,106 @@
-// import { useEffect, useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { GetJobs } from '../actions/Jobs';
-import { useSelector } from 'react-redux';
-import { Card } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { GetJobs } from '../actions/Jobs';
+import { Card, Row, Col } from 'react-bootstrap';
 
 const JobsComponent = () => {
+  const dispatch = useDispatch();
   const jobs = useSelector((state) => state.jobs.jobs);
+  const [job, setJob] = useState(false); // Usa stato locale per il singolo lavoro
+  useEffect(() => {
+    dispatch(GetJobs()); // Assicurati che dispatch venga chiamato correttamente
+    setJob(true);
+  }, [dispatch]);
 
   return (
     <div>
       <Card>
-        {jobs.slice(0, 10).map((job) => (
-          <div key={job._id}>
-            <h2 id='jobTitle'>{job.title}</h2>
-            <p dangerouslySetInnerHTML={{ __html: job.description }}></p>
-            <hr />
-          </div>
-        ))}
+        <div className=' px-3 divAcaso'>
+          <h2 className='titleJobCards'>Offerte di lavoro per te:</h2>
+          <p className=' subtitleJobsCard'>
+            In base al tuo profilo, alle tue preferenze e ad attività come
+            candidature, ricerche e salvataggi
+          </p>
+        </div>
+        <div style={{ maxHeight: '400px', overflowY: 'auto' }}>
+          {job ? (
+            jobs.slice(0, 10).map((job) => (
+              <div key={job._id} className=' px-3 d-flex  pt-1'>
+                <img
+                  src='https://placecats.com/neo_banana/300/300'
+                  className='workImage'
+                />
+                <div className=' container-fluid'>
+                  <h2 id='jobTitle'>{job.title}</h2>
+                  <p>
+                    {job.company_name} · {job.candidate_required_location}
+                  </p>
+                  <Row>
+                    <Col xs={8}>
+                      <p className=' text-start'>
+                        Category: {job.category}, job type:{' '}
+                        {job.job_type.replaceAll('_', ' ')}
+                      </p>
+                    </Col>
+                    <Col xs={4}>
+                      <p className=' text-end'>
+                        {job.publication_date.slice(0, 10)}
+                      </p>
+                    </Col>
+                  </Row>
+                  <hr />
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>No jobs available</p>
+          )}
+        </div>
+      </Card>
+      <Card className=' mt-3'>
+        <div className=' px-3 divAcaso'>
+          <h2 className='titleJobCards'>
+            Altre offerte di lavoro che potrebbero interessarti:
+          </h2>
+          <p className=' subtitleJobsCard'>
+            In base al tuo profilo, alle tue preferenze e ad attività come
+            candidature, ricerche e salvataggi
+          </p>
+        </div>
+        <div style={{ maxHeight: '400px', overflowY: 'auto' }}>
+          {job ? (
+            jobs.slice(11, 21).map((job) => (
+              <div key={job._id} className=' px-3 d-flex  pt-1'>
+                <img
+                  src='https://placecats.com/neo_2/300/300'
+                  className='workImage'
+                />
+                <div className=' container-fluid'>
+                  <h2 id='jobTitle'>{job.title}</h2>
+                  <p>
+                    {job.company_name} · {job.candidate_required_location}
+                  </p>
+                  <Row>
+                    <Col xs={8}>
+                      <p className=' text-start'>
+                        Category: {job.category}, job type:{' '}
+                        {job.job_type.replaceAll('_', ' ')}
+                      </p>
+                    </Col>
+                    <Col xs={4}>
+                      <p className=' text-end'>
+                        {job.publication_date.slice(0, 10)}
+                      </p>
+                    </Col>
+                  </Row>
+                  <hr />
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>No jobs available</p>
+          )}
+        </div>
       </Card>
     </div>
   );

ea30344eda120b90ac5da62a377ada468180608d
Reyett___ <reyrenna4@gmail.com>
2025-01-30 09:29:20 +0100

    ...

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,19 +1,19 @@
-import { Col, Container, Row } from 'react-bootstrap';
-import SideBarRightHome from './SideBarRightHome';
-import RightSide from './RightSideHome';
-import PostsComponent from './PostsComponent';
+import { Col, Container, Row } from "react-bootstrap";
+import SideBarRightHome from "./SideBarRightHome";
+import RightSide from "./RightSideHome";
+import PostsComponent from "./PostsComponent";
 
 function HomeComponent() {
   return (
-    <Container className='mt-5'>
-      <Row className='justify-content-center'>
-        <Col xs={12} md={3} className=' p-0 mt-4'>
+    <Container className="mt-5">
+      <Row className="justify-content-center">
+        <Col xs={12} md={2} className=" p-0 mt-4">
           <RightSide />
         </Col>
-        <Col xs={12} md={6} className=' p-0 mt-4 px-3'>
+        <Col xs={12} md={7} className=" p-0 mt-4 px-3">
           <PostsComponent />
         </Col>
-        <Col xs={12} md={3} className=' p-0 mt-4'>
+        <Col xs={12} md={3} className=" p-0 mt-4">
           <SideBarRightHome />
         </Col>
       </Row>

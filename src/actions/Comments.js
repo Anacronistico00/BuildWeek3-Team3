b50684b5e1b2fd904633dd33c03118f7ba24e057
Reyett___ <reyrenna4@gmail.com>
2025-01-31 11:07:12 +0100

    vabbè

--- src/actions/Comments.js
+++ src/actions/Comments.js
@@ -1,18 +1,18 @@
-export const { GET_COMMENTS } = 'GET_COMMENTS';
+export const { GET_COMMENTS } = "GET_COMMENTS";
 
 export const GetComments = () => async (dispatch) => {
   try {
     const response = await fetch(
-      'https://striveschool-api.herokuapp.com/api/comments',
+      "https://striveschool-api.herokuapp.com/api/comments",
       {
         headers: {
           Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzYwMGRkNTBlYTI4NjAwMTUyOGI5NTgiLCJpYXQiOjE3MzgzMTQzOTgsImV4cCI6MTczOTUyMzk5OH0.cFyUagIPza1RaPsEvSVmDa_F3b-CIfjcJNfE3Fhmjtg',
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzYwMGRkNTBlYTI4NjAwMTUyOGI5NTgiLCJpYXQiOjE3MzgzMTQzOTgsImV4cCI6MTczOTUyMzk5OH0.cFyUagIPza1RaPsEvSVmDa_F3b-CIfjcJNfE3Fhmjtg",
         },
       }
     );
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
     const data = await response.json();
     console.log(data);
@@ -22,6 +22,6 @@ export const GetComments = () => async (dispatch) => {
       payload: data,
     });
   } catch (error) {
-    console.error('Errore nel recupero dei commenti:', error);
+    console.error("Errore nel recupero dei commenti:", error);
   }
 };

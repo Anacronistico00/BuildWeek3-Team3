bf42f516eae54d3cfd9ae2d408140b4e727d2d2c
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-01-31 15:05:43 +0100

    Commit

--- src/actions/GetPosts.js
+++ src/actions/GetPosts.js
@@ -1,13 +1,13 @@
 export const GET_POSTS = 'GET_POSTS';
 
-export const fetchPosts = (token) => {
+export const fetchPosts = () => {
   return async (dispatch) => {
     try {
       const response = await fetch(
         'https://striveschool-api.herokuapp.com/api/posts/',
         {
           headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Nzk3NDI1ZTE2ZjYzNTAwMTVmZWNiNzYiLCJpYXQiOjE3Mzc5NjYxNzQsImV4cCI6MTczOTE3NTc3NH0.C54YDV4poWgVTe4vEBdnfY19L0nYPNupc4t-FcJn1m8`,
           },
         }
       );

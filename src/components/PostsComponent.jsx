3c3907c61c9c9d3f0b084bf7f2bfb7f9b6e636c7
Reyett___ <reyrenna4@gmail.com>
2025-01-31 10:54:18 +0100

    uguale a prima ma mi fa committare ok

--- src/components/PostsComponent.jsx
+++ src/components/PostsComponent.jsx
@@ -8,9 +8,9 @@ import {
   Row,
   Modal,
   FormControl,
-} from 'react-bootstrap';
-import { useState, useEffect } from 'react';
-import CreateNewPostComponent from './CreateNewPostComponent';
+} from "react-bootstrap";
+import { useState, useEffect } from "react";
+import CreateNewPostComponent from "./CreateNewPostComponent";
 import {
   ChatDots,
   Globe,
@@ -18,11 +18,11 @@ import {
   SendFill,
   ThreeDots,
   XLg,
-} from 'react-bootstrap-icons';
-import { BiShare } from 'react-icons/bi';
-import { useSelector } from 'react-redux';
+} from "react-bootstrap-icons";
+import { BiShare } from "react-icons/bi";
+import { useSelector } from "react-redux";
 
-const URL = 'https://striveschool-api.herokuapp.com/api/posts/';
+const URL = "https://striveschool-api.herokuapp.com/api/posts/";
 
 const PostsComponent = () => {
   const token = useSelector((state) => state.token.token);
@@ -32,7 +32,7 @@ const PostsComponent = () => {
   const [postToDelete, setPostToDelete] = useState(null);
   const [showEditModal, setShowEditModal] = useState(false);
   const [postToEdit, setPostToEdit] = useState(null);
-  const [editText, setEditText] = useState('');
+  const [editText, setEditText] = useState("");
 
   useEffect(() => {
     fetchPosts();
@@ -41,7 +41,7 @@ const PostsComponent = () => {
   const fetchPosts = async () => {
     try {
       const response = await fetch(URL, {
-        method: 'GET',
+        method: "GET",
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -49,21 +49,21 @@ const PostsComponent = () => {
 
       if (response.ok) {
         const data = await response.json();
-        console.log('SONO DATA', data);
+        console.log("SONO DATA", data);
         setPosts(data.reverse().slice(0, 30));
       } else {
-        throw new Error('Errore nel recupero dei dati');
+        throw new Error("Errore nel recupero dei dati");
       }
     } catch (error) {
       setError(error.message);
-      console.log('Errore nella fetch dei dati', error);
+      console.log("Errore nella fetch dei dati", error);
     }
   };
 
   const deletePost = async (postId) => {
     try {
       const response = await fetch(`${URL}${postId}`, {
-        method: 'DELETE',
+        method: "DELETE",
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -73,24 +73,24 @@ const PostsComponent = () => {
         setPosts(posts.filter((post) => post._id !== postId));
         setShowModal(false);
         setPostToDelete(null);
-        alert('Post eliminato');
+        alert("Post eliminato");
       } else {
-        throw new Error('Non puoi eliminare questo post');
+        throw new Error("Non puoi eliminare questo post");
       }
     } catch (error) {
       setError(error.message);
       alert(error.message);
-      console.log('Errore nella fetch dei dati', error);
+      console.log("Errore nella fetch dei dati", error);
     }
   };
 
   const editPost = async (postId, newText) => {
     try {
       const response = await fetch(`${URL}${postId}`, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
           Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ text: newText }),
       });
@@ -102,13 +102,13 @@ const PostsComponent = () => {
         );
         setShowEditModal(false);
         setPostToEdit(null);
-        setEditText('');
+        setEditText("");
       } else {
-        throw new Error('Non puoi modificare questo post');
+        throw new Error("Non puoi modificare questo post");
       }
     } catch (error) {
       alert(error.message);
-      console.log('Errore nella fetch dei dati', error);
+      console.log("Errore nella fetch dei dati", error);
     }
   };
 
@@ -149,57 +149,57 @@ const PostsComponent = () => {
             <p>Nessun post disponibile</p>
           ) : (
             posts.map((post) => (
-              <Card className='mt-2' key={post._id}>
+              <Card className="mt-2" key={post._id}>
                 <Card.Body>
-                  <div className='d-flex justify-content-between align-items-start'>
-                    <div className='d-flex justify-content-start align-items-center'>
+                  <div className="d-flex justify-content-between align-items-start">
+                    <div className="d-flex justify-content-start align-items-center">
                       <Card.Img
-                        variant='top'
-                        className='rounded-circle'
-                        style={{ width: '40px', height: '40px' }}
+                        variant="top"
+                        className="rounded-circle"
+                        style={{ width: "40px", height: "40px" }}
                         src={post.user.image}
                       />
 
-                      <div className='ms-1'>
+                      <div className="ms-1">
                         <Card.Title>{post.username}</Card.Title>
-                        <Card.Subtitle className='smallertext mb-2 text-muted align-baseline'>
+                        <Card.Subtitle className="smallertext mb-2 text-muted align-baseline">
                           {new Date(post.createdAt).toLocaleString()}
-                          <span className='ms-1'>
+                          <span className="ms-1">
                             <Globe />
                           </span>
                         </Card.Subtitle>
                       </div>
                     </div>
-                    <div className=' d-flex'>
+                    <div className=" d-flex">
                       <Dropdown>
-                        <Dropdown.Toggle className=' btn bg-transparent text-black border-0 p-0 me-3'>
+                        <Dropdown.Toggle className=" btn bg-transparent text-black border-0 p-0 me-3">
                           <ThreeDots />
                         </Dropdown.Toggle>
                         <DropdownMenu>
-                          <Dropdown.Item href='#/action-1'>Salva</Dropdown.Item>
-                          <Dropdown.Item href='#/action-2'>
+                          <Dropdown.Item href="#/action-1">Salva</Dropdown.Item>
+                          <Dropdown.Item href="#/action-2">
                             Copia link al post
                           </Dropdown.Item>
-                          <Dropdown.Item href='#/action-3'>
+                          <Dropdown.Item href="#/action-3">
                             Incorpora questo post
                           </Dropdown.Item>
-                          <Dropdown.Item href='#/action-3'>
+                          <Dropdown.Item href="#/action-3">
                             Non mi interessa
                           </Dropdown.Item>
                           <Dropdown.Item onClick={() => handleEdit(post)}>
                             Modifica Post
                           </Dropdown.Item>
-                          <Dropdown.Item href='#/action-3'>
+                          <Dropdown.Item href="#/action-3">
                             Smetti di seguire {post.username}
                           </Dropdown.Item>
-                          <Dropdown.Item href='#/action-3'>
+                          <Dropdown.Item href="#/action-3">
                             Segnala post
                           </Dropdown.Item>
                         </DropdownMenu>
                       </Dropdown>
                       <div>
                         <Button
-                          className='bg-transparent text-black border-0 p-0 me-3'
+                          className="bg-transparent text-black border-0 p-0 me-3"
                           onClick={() => handleDelete(post._id)}
                         >
                           <XLg />
@@ -208,23 +208,23 @@ const PostsComponent = () => {
                     </div>
                   </div>
 
-                  <div className=' m-4 '>
+                  <div className=" m-4 ">
                     <Card.Text>{post.text}</Card.Text>
                   </div>
                   <div>
-                    <img src={post.image} className=' w-100 img-fluid' />
+                    <img src={post.image} className=" w-100 img-fluid" />
                   </div>
-                  <div className=' d-flex justify-content-between border-top p-2 mt-4'>
-                    <Button className=' text-secondary bg-transparent border-0'>
+                  <div className=" d-flex justify-content-between border-top p-2 mt-4">
+                    <Button className=" text-secondary bg-transparent border-0">
                       <HandThumbsUp /> <span>Consiglia</span>
                     </Button>
-                    <Button className=' text-secondary bg-transparent border-0'>
+                    <Button className=" text-secondary bg-transparent border-0">
                       <ChatDots /> <span>Commenta</span>
                     </Button>
-                    <Button className=' text-secondary bg-transparent border-0'>
+                    <Button className=" text-secondary bg-transparent border-0">
                       <BiShare /> <span>Diffondi il post</span>
                     </Button>
-                    <Button className=' text-secondary bg-transparent border-0'>
+                    <Button className=" text-secondary bg-transparent border-0">
                       <SendFill /> <span>Invia</span>
                     </Button>
                   </div>
@@ -243,10 +243,10 @@ const PostsComponent = () => {
         </Modal.Header>
         <Modal.Body>Sei sicuro di voler eliminare questo post</Modal.Body>
         <Modal.Footer>
-          <Button variant='secondary' onClick={() => setShowModal(false)}>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
             Annulla
           </Button>
-          <Button variant='danger' onClick={confirmDelete}>
+          <Button variant="danger" onClick={confirmDelete}>
             Elimina
           </Button>
         </Modal.Footer>
@@ -259,16 +259,16 @@ const PostsComponent = () => {
         </Modal.Header>
         <Modal.Body>
           <FormControl
-            as='textarea'
+            as="textarea"
             value={editText}
             onChange={(e) => setEditText(e.target.value)}
           />
         </Modal.Body>
         <Modal.Footer>
-          <Button variant='secondary' onClick={() => setShowEditModal(false)}>
+          <Button variant="secondary" onClick={() => setShowEditModal(false)}>
             Annulla
           </Button>
-          <Button variant='primary' onClick={confirmEdit}>
+          <Button variant="primary" onClick={confirmEdit}>
             Salva
           </Button>
         </Modal.Footer>

5c160e78db281d4c909c428a66eea5875efe8935
Reyett___ <reyrenna4@gmail.com>
2025-01-31 16:19:02 +0100

    d

--- src/components/PostsComponent.jsx
+++ src/components/PostsComponent.jsx
@@ -12,26 +12,15 @@ import {
 import { useState, useEffect } from "react";
 import CreateNewPostComponent from "./CreateNewPostComponent";
 import {
-  Bookmark,
   ChatDots,
-  CodeSlash,
-  EyeSlashFill,
-  FlagFill,
   Globe,
   HandThumbsUp,
-  Link45deg,
-  Pencil,
-  PlusCircleFill,
   SendFill,
   ThreeDots,
   XLg,
 } from "react-bootstrap-icons";
 import { BiShare } from "react-icons/bi";
-import { useDispatch, useSelector } from "react-redux";
-import { GetComments, postHomeComment } from "../actions/Comments";
-import { FaRegImage } from "react-icons/fa";
-import { FaRegFaceSmile } from "react-icons/fa6";
-import { fetchPosts } from "../actions/GetPosts";
+import { useSelector } from "react-redux";
 
 const URL = "https://striveschool-api.herokuapp.com/api/posts/";
 
@@ -44,40 +33,12 @@ const PostsComponent = () => {
   const [showEditModal, setShowEditModal] = useState(false);
   const [postToEdit, setPostToEdit] = useState(null);
   const [editText, setEditText] = useState("");
-  const dispatch = useDispatch();
-  const comments = useSelector((state) => state.comments);
-  const profile = useSelector((state) => state.profileInfo);
-  const [commentValues, setCommentValues] = useState({});
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleCommentChange = (postId, value) => {
-    setCommentValues((prevValues) => ({
-      ...prevValues,
-      [postId]: value,
-    }));
-  };
-
-  const handlePostComment = (postId) => {
-    const commentValue = commentValues[postId];
-    if (commentValue) {
-      dispatch(postHomeComment(token, commentValue, postId));
-      setCommentValues((prevValues) => ({
-        ...prevValues,
-        [postId]: "",
-      }));
-    }
-  };
 
   useEffect(() => {
-    fetchPostsInternal();
-    dispatch(GetComments());
+    fetchPosts();
   }, []);
 
-  useEffect(() => {
-    dispatch(fetchPosts(token));
-  }, [dispatch]);
-
-  const fetchPostsInternal = async () => {
+  const fetchPosts = async () => {
     try {
       const response = await fetch(URL, {
         method: "GET",
@@ -174,11 +135,15 @@ const PostsComponent = () => {
     }
   };
 
+  if (error) {
+    return <div>Errore: {error}</div>;
+  }
+
   return (
     <Container>
       <Row>
         <Col>
-          <CreateNewPostComponent />
+          <CreateNewPostComponent fetchPosts={fetchPosts} />
 
           {posts.length === 0 ? (
             <p>Nessun post disponibile</p>
@@ -208,38 +173,30 @@ const PostsComponent = () => {
                     <div className=" d-flex">
                       <Dropdown>
                         <Dropdown.Toggle className=" btn bg-transparent text-black border-0 p-0 me-3">
-                          <ThreeDots className=" me-2" />
+                          <ThreeDots />
                         </Dropdown.Toggle>
-                        <DropdownMenu
-                          style={{ width: "240px", height: "280px" }}
-                          className=" py-3"
-                        >
-                          <Dropdown.Item href="#/action-1">
-                            <Bookmark className=" me-2" /> Salva
-                          </Dropdown.Item>
+                        <DropdownMenu>
+                          <Dropdown.Item href="#/action-1">Salva</Dropdown.Item>
                           <Dropdown.Item href="#/action-2">
-                            <Link45deg className=" me-2" /> Copia link al post
+                            Copia link al post
                           </Dropdown.Item>
                           <Dropdown.Item href="#/action-3">
-                            <CodeSlash className=" me-2" /> Incorpora questo
-                            post
+                            Incorpora questo post
                           </Dropdown.Item>
                           <Dropdown.Item href="#/action-3">
-                            <EyeSlashFill className=" me-2" /> Non mi interessa
+                            Non mi interessa
                           </Dropdown.Item>
                           <Dropdown.Item onClick={() => handleEdit(post)}>
-                            <Pencil className=" me-2" /> Modifica Post
+                            Modifica Post
                           </Dropdown.Item>
                           <Dropdown.Item href="#/action-3">
-                            <PlusCircleFill className=" me-2" /> Smetti di
-                            seguire {post.username}
+                            Smetti di seguire {post.username}
                           </Dropdown.Item>
                           <Dropdown.Item href="#/action-3">
-                            <FlagFill className=" me-2" /> Segnala post
+                            Segnala post
                           </Dropdown.Item>
                         </DropdownMenu>
                       </Dropdown>
-
                       <div>
                         <Button
                           className="bg-transparent text-black border-0 p-0 me-3"
@@ -251,7 +208,7 @@ const PostsComponent = () => {
                     </div>
                   </div>
 
-                  <div className=" m-2 ">
+                  <div className=" m-4 ">
                     <Card.Text>{post.text}</Card.Text>
                   </div>
                   <div>
@@ -261,10 +218,7 @@ const PostsComponent = () => {
                     <Button className=" text-secondary bg-transparent border-0">
                       <HandThumbsUp /> <span>Consiglia</span>
                     </Button>
-                    <Button
-                      className=" text-secondary bg-transparent border-0"
-                      onClick={() => setIsOpen(!isOpen)}
-                    >
+                    <Button className=" text-secondary bg-transparent border-0">
                       <ChatDots /> <span>Commenta</span>
                     </Button>
                     <Button className=" text-secondary bg-transparent border-0">
@@ -275,101 +229,6 @@ const PostsComponent = () => {
                     </Button>
                   </div>
                 </Card.Body>
-                {isOpen && (
-                  <Card.Footer>
-                    <div className="d-flex align-items-center justify-content-between position-relative">
-                      <Row className=" w-100">
-                        <Col xs={9}>
-                          <Row>
-                            <Col xs={1} className=" mt-1">
-                              <img
-                                src={profile.profileInfo.image}
-                                alt=""
-                                className="rounded-circle me-2"
-                                style={{ width: "30px", height: "30px" }}
-                              />
-                            </Col>
-                            <Col xs={11}>
-                              {" "}
-                              <FormControl
-                                as="textarea"
-                                value={commentValues[post._id] || ""}
-                                onChange={(e) =>
-                                  handleCommentChange(post._id, e.target.value)
-                                }
-                                className="rounded-pill"
-                                style={{
-                                  height: "40px",
-                                }}
-                              />
-                            </Col>
-                          </Row>
-                        </Col>
-                        <Col xs={3}>
-                          {commentValues[post._id] && (
-                            <Row>
-                              <Col
-                                xs={6}
-                                className=" text-center fs-5 text-secondary"
-                              >
-                                <FaRegFaceSmile />
-                              </Col>
-                              <Col
-                                xs={6}
-                                className=" text-center fs-5 text-secondary"
-                              >
-                                <FaRegImage className="ms-3" />
-                              </Col>
-                              <Col xs={12} className=" text-center">
-                                <Button
-                                  className=" bg-primary border-0 text-white rounded-pill mt-1"
-                                  onClick={() => handlePostComment(post._id)}
-                                >
-                                  Pubblica
-                                </Button>
-                              </Col>
-                            </Row>
-                          )}
-                        </Col>
-                      </Row>
-
-                      <div className="commentIcons d-flex align-items-center position-absolute fs-5 text-secondary"></div>
-                    </div>
-                    {comments.comments.length > 0 &&
-                      comments.comments
-                        .filter((comment) => comment.elementId === post._id)
-                        .map((comment) => (
-                          <div
-                            key={comment._id}
-                            style={{ maxHeight: "200px", overflowY: "hidden" }}
-                          >
-                            <div className="d-flex align-items-center justify-content-between">
-                              <div className="d-flex align-items-center">
-                                <img
-                                  src={post.user.image}
-                                  alt=""
-                                  className="rounded-circle"
-                                  style={{ width: "30px", height: "30px" }}
-                                />
-                                <p className="fw-medium ms-2">
-                                  {comment.author}
-                                </p>
-                              </div>
-
-                              <div>
-                                <p>
-                                  {post.user.createdAt.slice(5, 10)} alle{" "}
-                                  {post.user.createdAt.slice(11, 16)}
-                                </p>
-                              </div>
-                            </div>
-                            <div>
-                              <Card.Text>{comment.comment}</Card.Text>
-                            </div>
-                          </div>
-                        ))}
-                  </Card.Footer>
-                )}
               </Card>
             ))
           )}
